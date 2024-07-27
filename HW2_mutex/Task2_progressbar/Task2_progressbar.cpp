e3462ef2aaeb2a8fc7f928cae945ed7b60c9d7c8
alexandra_f <avfriesen@mail.ru>
2024-07-27 20:27:05 +0300

    Section7 task 2, 3 changed

--- HW2_mutex/Task2_progressbar/Task2_progressbar.cpp
+++ HW2_mutex/Task2_progressbar/Task2_progressbar.cpp
@@ -5,19 +5,40 @@
 #include <Windows.h> // WinApi
 #include<chrono>
 #include<mutex>
+#include <random>
 
 std::mutex m;
-auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+//auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-// устанавливает курсор в консоли на указанную позицию
-void set_cursor(int x, int y)
+class consol_parameter
 {
-    COORD point;
-    point.X = x;
-    point.Y = y;
-    SetConsoleCursorPosition(handle, point);
-    //SetConsoleCursorPosition(handle, { static_cast<SHORT>(x), static_cast<SHORT>(y) });
-}
+public:
+    static void SetColor(int text, int background)
+    {
+        SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+    }
+    static void SetPosition(int x, int y)
+    {
+        COORD point;
+        point.X = x;
+        point.Y = y;
+        SetConsoleCursorPosition(hStdOut, point);
+    }
+private:
+    static HANDLE hStdOut;
+};
+HANDLE consol_parameter::hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+//// устанавливает курсор в консоли на указанную позицию
+//void set_cursor(int x, int y)
+//{
+//    COORD point;
+//    point.X = x;
+//    point.Y = y;
+//    SetConsoleCursorPosition(handle, point);
+//    //SetConsoleCursorPosition(handle, { static_cast<SHORT>(x), static_cast<SHORT>(y) });
+//}
 
 // отрисовывает прогресс-бар в консоли по указанным координатам
 // x - х-координата для прогресс-бара
@@ -27,17 +48,17 @@ void set_cursor(int x, int y)
 
 void render_progress_bar(int x, int y, int barWidth, double progress)
 {
-    
-    set_cursor(x, y);
+    consol_parameter::SetPosition(x, y);
+    int pos = barWidth * progress;
 
-    int fill_w = static_cast<int>(barWidth * progress);
-    int empty_w = barWidth - fill_w;
-
-    for (int i = 0; i < fill_w; i++)
-    std::cout << ">";
-
-    for (int i = 0; i < empty_w; i++)
-     std::cout << " ";
+    for (int i = 0; i <= barWidth; i++) {
+        if (i == 0) std::cout << "[";
+        if (i < pos) std::cout << "=";
+        else if (i == pos) std::cout << ">";
+        else std::cout << " ";
+        if (i == barWidth) std::cout << "]";
+    }
+  
 }
 
 void process_imitation(int indx, int num, int count) {
@@ -52,62 +73,72 @@ void process_imitation(int indx, int num, int count) {
     int last_index = start;
     double progress = 0;
 
-    int progress_y = 2 +  indx;
-    int progress_x = 1;
+    int progress_y = 1+indx;
+    int progress_x = 0;
 
-    //m.lock();
-    
-    set_cursor(progress_x, progress_y);
-    std::cout <<indx<<". "<< std::this_thread::get_id() << " " << "[";
+    m.lock();
+    consol_parameter::SetPosition(progress_x, progress_y);
+    std::cout << indx << ". " << std::this_thread::get_id();
+    m.unlock();
 
-    set_cursor(progress_x +barWidth+11, progress_y);
-    std::cout << "]";
-    
+     //consol_parameter::SetPosition(progress_x, progress_y);
+    //std::cout << indx << ". " << std::this_thread::get_id();
+
     auto start_t = std::chrono::high_resolution_clock::now();
 
-   
-    // имитация изменения прогресса
-    while (last_index < end)
-    {
-       render_progress_bar(progress_x+10, progress_y, barWidth, progress);
+  // 
+  //  // имитация изменения прогресса
+    while (last_index<end)
+    {   
+        // случайная задержка времени 
+        std::random_device t;
+        std::uniform_int_distribution<int> dist(0, 10);
+        int time = dist(t);
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+
+        std::lock_guard<std::mutex> grd(m);
+        render_progress_bar(progress_x+10, progress_y, barWidth, progress);
+
        last_index += 1;
        progress = (static_cast<double>(last_index - start) / static_cast<double>(end - start));
-    }
+    } 
 
     auto end_t = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double>time = end_t - start_t;
 
-   set_cursor(progress_x + barWidth + 13, progress_y);
-   std::cout << " " << time.count() << std::endl;
-  // m.unlock();
-   //std::cout << "\n";
+    consol_parameter::SetPosition(progress_x + barWidth + 19, progress_y);
+    std::cout << " " << time.count();
+ 
+  std::cout << "\n";
 }
 
 int main()
 {
-    set_cursor(0, 0);
+    consol_parameter::SetPosition(0, 0);
 
     // поток отрисовки
-    int number_of_th = 5;
-    int count = 5000;
+    int number_of_th = 3;
+    int count = 1000;
 
 
     std::vector<std::thread> ths;
     ths.reserve(number_of_th);
 
-    std::cout << "#" << "\t" << "id" << "\t " << "PrBar" << "\t\t " << "time" << std::endl;
+    std::cout << "#" << "  " << "id" << "\t\t" << "PrBar" << "\t\t\t " << "time";
 
     for (int i = 0; i < number_of_th; i++) {
         int length = count / number_of_th; 
+        //std::cout << i << std::endl;
+       // consol_parameter::SetPosition(1, 2+i);
         ths.emplace_back(process_imitation, i, number_of_th, count);
-        std::cout << "\n\n";
+        //std::cout << "\n";
        
     }
 
     for (auto& thread : ths) {          // (2)
-        thread.detach();
+        thread.join();
     }
 
-    set_cursor(0,10);
+    consol_parameter::SetPosition(0,6);
     return 0;
 }
