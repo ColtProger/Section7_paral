e08504a3a7755bebe1fa96c17147d00083443909
alexandra_f <avfriesen@mail.ru>
2024-07-31 13:01:43 +0300

    Section7, async task2.v1 added

--- HW3_asynx/Task2_for_each/Task2_for_each.cpp
+++ HW3_asynx/Task2_for_each/Task2_for_each.cpp
@@ -5,27 +5,25 @@
 #include <iostream>
 #include <chrono>
 
-void accumulate(std::vector<int>::iterator first,
+int print(int elem) {
+
+    std::cout << elem << " ";
+    return 0;
+
+}
+
+void for_each(std::vector<int>::iterator first,
     std::vector<int>::iterator last,
-    std::promise<int> accumulate_promise)
-{
-    int sum = std::accumulate(first, last, 0);
-    accumulate_promise.set_value(sum);  // оповестить future
+    int(*func)(int))
+{    
+    for (auto it = first; it != last; ++it)
+        func(*it);
 }
 
 int main()
 {
     // Демонстрация использования promise<int> для передачи результата между потоками.
     std::vector<int> numbers = { 1, 2, 3, 4, 5, 6 };
-    std::promise<int> accumulate_promise;
-    std::future<int> accumulate_future = accumulate_promise.get_future();
-    std::thread work_thread(accumulate, numbers.begin(), numbers.end(),
-        std::move(accumulate_promise));
-
-    // future::get() будет ждать, пока future не будет иметь допустимый результат,
-    // после чего запросит его.
-    // Вызов wait() перед get() не требуется.
-    //accumulate_future.wait();  // ждать результата
-    std::cout << "result=" << accumulate_future.get() << '\n';
-    work_thread.join();  // ждем завершения потока
+    
+    for_each(numbers.begin(), numbers.end(), &print);
 }
